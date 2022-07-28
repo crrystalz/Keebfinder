8701834dc47c5d3745eebce4aec896ea70c5e305
crystalz <lichupatnaik@gmail.com>
2022-07-28 11:22:52 -0700

    why discordjs v14 smh, made command handler and other basic stuff

---
+++ src/events/client/ready.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name: 'ready',
+    once: true,
+    async execute(client) {
+        const activities = [
+            "made by crrystalz#0001",
+            "/help (NOT WORKING)"
+        ]
+        console.log('Keebfinder is online!');
+        var index = 0;
+        setInterval(() => {
+            const newActivity = activities[index];
+            client.user.setActivity(newActivity);
+
+            if (index == 0) {
+                index = 1;
+            }
+            else {
+                index = 0;
+            }
+
+        }, 5000);
+    }
+}
